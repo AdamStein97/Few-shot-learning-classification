38db93ea13058d6d778f932434c2875e3ea0fdb1
Mike <adam.m.stein97@gmail.com>
2022-06-16 20:01:36 +0100

    set up fine-tuner

--- few_shot_learning_classification/bart_predictor.py
+++ few_shot_learning_classification/bart_predictor.py
@@ -1,51 +1,103 @@
-from typing import Optional
+import math
+from typing import List, Optional, Tuple
 
 import numpy as np
-import pandas as pd
+import torch
+from tqdm import tqdm
+from transformers import BartForSequenceClassification, BartTokenizer
 
 from few_shot_learning_classification.config import Config
-from few_shot_learning_classification.model_fine_tuner import ModelFineTuner
-from few_shot_learning_classification.zero_shot_classifier import \
-    ZeroShotClassifier
+from few_shot_learning_classification.utils import create_text_pairs
 
 
 class BartPredictor:
-    """
-    High-level class to manage inference and training of the zero-shot classifier
-    """
-
-    def __init__(self, path_to_trained_model=None, **kwargs):
-        self.classifier = ZeroShotClassifier(path_to_trained_model)
-
-    def run_inference(
+    def __init__(
         self,
-        answers: np.array,
-        search_term: str,
-        hypothesis: Optional[str] = None,
+        path_to_trained_model=None,
+    ):
+        if path_to_trained_model is None:
+            path_to_trained_model = "facebook/bart-large-mnli"
+
+        device_str = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.device = torch.device(device_str)
+        self.detach = (
+            lambda x: x.cpu().detach().numpy()
+            if torch.cuda.is_available()
+            else x.detach().numpy()
+        )
+
+        self._init_model(path_to_trained_model)
+
+    def _init_model(self, path_to_trained_model):
+        self.tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-mnli")
+        model = BartForSequenceClassification.from_pretrained(path_to_trained_model)
+        self.model = model
+        if torch.cuda.is_available():
+            self.model = self.model.cuda()
+
+        self.model.eval()
+
+    def _predict_on_batch(
+        self,
+        text_batch: List[str],
+        labels_batch: List[str],
+        hypothesis: str,
+        threshold: float = 0.99,
+        max_tokenizer_length: int = Config.MAX_LENGTH,
+    ) -> Tuple[List[int], List[float]]:
+
+        text_pairs1, text_pairs2 = create_text_pairs(
+            hypothesis, text_batch, labels_batch
+        )
+
+        inputs = self.tokenizer(
+            text=text_pairs1,
+            text_pair=text_pairs2,
+            return_tensors="pt",
+            truncation="only_first",
+            padding=True,
+            max_length=max_tokenizer_length,
+        )
+
+        inputs.to(self.device)
+
+        with torch.no_grad():
+            logits = self.model(**inputs)[0].cpu()
+
+        entail_contr_logits = logits[..., [0, 2]]
+        probabilities = np.exp(entail_contr_logits) / np.exp(entail_contr_logits).sum(
+            -1, keepdims=True
+        )
+        probabilities = self.detach(probabilities[..., 1])
+
+        labels = probabilities > threshold
+        labels = [int(label) for label in labels]
+
+        return labels, probabilities.tolist()
+
+    def predict(
+        self,
+        texts: List[str],
+        labels: List[str],
+        hypothesis: Optional[List[str]] = None,
         batch_size: int = Config.BART_INFERENCE_BS,
         threshold: float = 0.99,
         max_tokenizer_length: int = Config.MAX_LENGTH,
-    ) -> pd.DataFrame:
+    ) -> Tuple[List[int], List[float]]:
+        labels = []
+        probabilities = []
 
-        nodes = [search_term] * len(answers)
-
-        hypotheses = [hypothesis] * len(answers)
-
-        labels, probs = self.classifier.predict(
-            answers,
-            nodes,
-            hypothesis=hypotheses,
-            batch_size=batch_size,
-            threshold=threshold,
-            max_tokenizer_length=max_tokenizer_length,
-        )
-        df = pd.DataFrame(
-            {"answers": answers, "pred_probability": probs, "pred_labels": labels}
-        )
-
-        return df
-
-    def run_fine_tune(self, train_df: pd.DataFrame, **kwargs):
-        fine_tuner = ModelFineTuner(self.classifier.model)
-        fine_tuned_model = fine_tuner.fine_tune(train_df, **kwargs)
-        self.classifier.set_model(fine_tuned_model)
+        for i in tqdm(range(math.ceil(len(texts) / batch_size))):
+            if i * batch_size < len(texts):
+                text_batch = texts[i * batch_size : (i + 1) * batch_size]
+                label_batch = labels[i * batch_size : (i + 1) * batch_size]
+                batch_labels, batch_probs = self._predict_on_batch(
+                    text_batch,
+                    label_batch,
+                    hypothesis=hypothesis,
+                    threshold=threshold,
+                    max_tokenizer_length=max_tokenizer_length,
+                )
+                labels += batch_labels
+                probabilities += batch_probs
+        return labels, probabilities
--- few_shot_learning_classification/model_fine_tuner.py
+++ few_shot_learning_classification/model_fine_tuner.py
@@ -34,49 +34,51 @@ class ModelFineTuner:
     into few-shot model
     """
 
-    def __init__(
-        self,
-        model: BartForSequenceClassification,
-        model_str: str = "facebook/bart-large-mnli",
-    ):
-        self.tokenizer = BartTokenizer.from_pretrained(model_str)
-        self.model = model
-
     def fine_tune(
         self,
         train_df: pd.DataFrame,
         hypothesis: str,
+        model_save_path: str,
         num_train_epochs: int = Config.FINE_TUNE_EPOCHS,
         batch_size: int = Config.FINE_TUNE_BS,
         **kwargs
     ) -> BartForSequenceClassification:
+
+        model = BartForSequenceClassification.from_pretrained(
+            "facebook/bart-large-mnli"
+        )
+
         training_args = TrainingArguments(
             output_dir="./results",
             num_train_epochs=num_train_epochs,
             per_device_train_batch_size=batch_size,
             learning_rate=Config.FINE_TUNE_LR,
         )
-        train_encodings = self._prepare_data(train_df, hypothesis)
+        train_encodings = self._process_data(train_df, hypothesis)
         train_dataset = MNLIDataset(train_encodings, train_df["labels"].values)
 
         trainer = Trainer(
-            model=self.model,  # the instantiated 🤗 Transformers model to be trained
+            model=model,  # the instantiated 🤗 Transformers model to be trained
             args=training_args,  # training arguments, defined above
-            train_dataset=train_dataset,  # training dataset
-            eval_dataset=train_dataset,
+            train_dataset=train_dataset,
+            eval_dataset=train_dataset,  # Hugging face requires an eval dataset
         )
 
         trainer.train()
 
-        return self.model
+        model.save_pretrained(model_save_path)
 
-    def _prepare_data(self, df: pd.DataFrame, hypothesis) -> Dict[str, np.array]:
+        return model
+
+    def _process_data(self, df: pd.DataFrame, hypothesis) -> Dict[str, np.array]:
+
+        tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-mnli")
         texts = df["text"].values
         labels = df["label"].values
 
         text_pairs1, text_pairs2 = create_text_pairs(hypothesis, texts, labels)
 
-        inputs = self.tokenizer(
+        inputs = tokenizer(
             text=text_pairs1,
             text_pair=text_pairs2,
             truncation="only_first",
